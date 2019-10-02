8a591705b2f661f45d3cb56138cb325e70a74ca5
Asrar Homod <45241728+AsrarH@users.noreply.github.com>
2019-10-02 16:32:49 +0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -28,7 +28,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // RETURNS
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
   app.get( "/filteredimage", async ( req, res ) => {
-    let image_url = req.query.image_url;
+    let image_url:string = req.query.image_url;
     if (image_url) {
       filterImageFromURL(image_url).then((response) => {
         res.sendFile(response);
